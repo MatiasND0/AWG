109024a14b815dfa7f848a989f5cc775d511bbcc
MatiasND0 <matias.nd99@gmail.com>
2023-10-25 21:18:50 -0300

    sine config window

--- RPI_code/mvc.cpp
+++ RPI_code/mvc.cpp
@@ -21,8 +21,15 @@ enum WND_ID
 	ID_BUTTON_3,
 	ID_BUTTON_4,
 	ID_BUTTON_5,
-	ID_BUTTON_6,
-	ID_LIST_BOX,
+	ID_BUTTON_10,
+	ID_LABEL_11,
+	ID_LABEL_12,
+	ID_LABEL_13,
+	ID_LABEL_14,
+	ID_SPIN_BOX_11,
+	ID_SPIN_BOX_12,
+	ID_SPIN_BOX_13,
+	ID_SPIN_BOX_14,
 	ID_DIALOG
 };
 
@@ -41,9 +48,8 @@ class MVC : public c_wnd
 	}
 	virtual void on_paint(void)
 	{
-		m_surface->draw_rect(4, 34, 56, 285,GL_RGB(255, 255, 255),Z_ORDER_LEVEL_2);
-		for(int i=0;i<70;i++)	
-			m_surface->draw_pixel((120+i),(25+8*sin(i*3.14/15)),GL_RGB(255, 255, 255),Z_ORDER_LEVEL_0);
+		c_dialog::open_dialog((c_dialog*)get_wnd_ptr(ID_DIALOG));
+		m_surface->draw_rect(4, 34, 81, 286,GL_RGB(255, 255, 255),Z_ORDER_LEVEL_2);
 	}
 
 	void on_button_clicked(int ctrl_id, int param)
@@ -57,34 +63,41 @@ class c_my_dialog : public c_dialog
 {
 	virtual void on_init_children()
 	{
-	
-		c_list_box  *list_box = (c_list_box*)get_wnd_ptr(ID_LIST_BOX);
-
-		list_box->set_on_change((WND_CALLBACK)&c_my_dialog::on_listbox_confirm);
-		list_box->clear_item();
-		list_box->add_item((char*)"Sine");
-		list_box->add_item((char*)"Square");
-		list_box->add_item((char*)"Sawtooth");
-		list_box->select_item(0);
-		
-		c_button* s_button_1 = (c_button*)get_wnd_ptr(ID_BUTTON_6);
-		s_button_1->set_on_click((WND_CALLBACK)&c_my_dialog::on_button_clicked);
-		
-	}
-	void on_listbox_confirm(int ctrl_id, int param)
-	{
+		c_spin_box *c_spin_box_1 = (c_spin_box*)get_wnd_ptr(ID_SPIN_BOX_11);
+		c_spin_box *c_spin_box_2 = (c_spin_box*)get_wnd_ptr(ID_SPIN_BOX_12);
+		c_spin_box *c_spin_box_3 = (c_spin_box*)get_wnd_ptr(ID_SPIN_BOX_13);
+		c_spin_box *c_spin_box_4 = (c_spin_box*)get_wnd_ptr(ID_SPIN_BOX_14);
+		c_spin_box_1->set_value(1000);
+		c_spin_box_2->set_value(0);
+		c_spin_box_3->set_value(5);
+		c_spin_box_4->set_value(0);
 	}
 
 	virtual void on_paint(void)
 	{
-		m_surface->draw_rect(51, 51, 240, 240,GL_RGB(255, 255, 255),Z_ORDER_LEVEL_2);
+		m_surface->draw_rect(94, 34, 446, 150,GL_RGB(255, 255, 255),Z_ORDER_LEVEL_2);
+		m_surface->draw_rect(94, 149, 446, 286,GL_RGB(255, 255, 255),Z_ORDER_LEVEL_2);
+		m_surface->fill_rect(95, 150, 445, 285,GL_RGB(25, 25, 25),Z_ORDER_LEVEL_2);
+		m_surface->draw_line(95, 217, 445, 217,GL_RGB(100, 100, 100),Z_ORDER_LEVEL_2);
+		m_surface->draw_line(95, 184, 445, 184,GL_RGB(50, 50, 50),Z_ORDER_LEVEL_2);
+		m_surface->draw_line(95, 251, 445, 251,GL_RGB(50, 50, 50),Z_ORDER_LEVEL_2);
+
+		for(int i=0;i<350;i++)	
+			m_surface->draw_line((95+i),(217-50*sin(i*3.14/88)),(95+i+1),(217-50*sin((i+1)*3.14/88)),GL_RGB(255, 255, 0),Z_ORDER_LEVEL_2);
+
+		m_surface->draw_line(95+88, 150, 95+88, 285,GL_RGB(50, 50, 50),Z_ORDER_LEVEL_2);
+		m_surface->draw_line(95+175, 150, 95+175, 285,GL_RGB(50, 50, 50),Z_ORDER_LEVEL_2);
+		m_surface->draw_line(95+265, 150, 95+265, 285,GL_RGB(50, 50, 50),Z_ORDER_LEVEL_2);
+	
+		m_surface->draw_rect(94, 34, 446, 150,GL_RGB(255, 255, 255),Z_ORDER_LEVEL_2);
+
 	}
 
 	void on_button_clicked(int ctrl_id, int param)
 	{
 		switch (ctrl_id)
 		{
-		case ID_BUTTON_6:
+		case ID_BUTTON_10:
 			c_dialog::close_dialog(m_surface);
 			break;
 		default:
@@ -97,26 +110,31 @@ static MVC mvc;
 static c_button main_button_1,main_button_2,main_button_3,main_button_4,main_button_5;
 
 static c_my_dialog s_my_dialog;
-static c_list_box s_list_box;
-static c_button s_button_1;
+static c_label s_label_1, s_label_2, s_label_3, s_label_4;
+static c_spin_box s_spin_box_1,s_spin_box_2,s_spin_box_3,s_spin_box_4;
 
 WND_TREE s_dialog_widgets[] =
 {
-
-	{ &s_button_1,	ID_BUTTON_6,"<-",	0, 0, 50, 50},
-	{ &s_list_box,	ID_LIST_BOX,"listBox",	50, 0, 190, 50},
+	{ &s_label_1,	ID_LABEL_11,	"Frequency",		15, 0+7, 75, 25},
+	{ &s_label_2,	ID_LABEL_12,	"Phase",			15, 25+7, 75, 25},
+	{ &s_label_3,	ID_LABEL_13,	"Amplitude",		15, 50+7, 75, 25},
+	{ &s_label_4,	ID_LABEL_14,	"Offset",			15, 75+7, 75, 25},
+	{ &s_spin_box_1,ID_SPIN_BOX_11,		"1000 Hz",		100, 0+7, 75, 25},
+	{ &s_spin_box_2,ID_SPIN_BOX_12,		"0 deg",				100, 25+7, 75, 25},
+	{ &s_spin_box_3,ID_SPIN_BOX_13,		"5 Vpp",			100, 50+7, 75, 25},
+	{ &s_spin_box_4,ID_SPIN_BOX_14,		"0 V",				100, 75+7, 75, 25},
 	{NULL, 0 , 0, 0, 0, 0, 0}
 };
 
 WND_TREE main_window[] =
 {
-	{ &main_button_1,	ID_BUTTON_1,	"Sine",		5, 35, 50, 50},
-	{ &main_button_2,	ID_BUTTON_2,	"Ramp",		5, 85, 50, 50},
-	{ &main_button_3,	ID_BUTTON_3,	"Square",	5, 135, 50, 50},
-	{ &main_button_4,	ID_BUTTON_4,	"Pulse",	5, 185, 50, 50},
-	{ &main_button_5,	ID_BUTTON_5,	"Noise",	5, 235, 50, 50},
+	{ &main_button_1,	ID_BUTTON_1,	"Sine",		5, 35, 75, 50},
+	{ &main_button_2,	ID_BUTTON_2,	"Ramp",		5, 85, 75, 50},
+	{ &main_button_3,	ID_BUTTON_3,	"Square",	5, 135, 75, 50},
+	{ &main_button_4,	ID_BUTTON_4,	"Pulse",	5, 185, 75, 50},
+	{ &main_button_5,	ID_BUTTON_5,	"Noise",	5, 235, 75, 50},
 
-	{ &s_my_dialog,	ID_DIALOG,	"Dialog",	51, 51, 240, 240, s_dialog_widgets},
+	{ &s_my_dialog,	ID_DIALOG,	"Dialog",	95, 35, 445, 285, s_dialog_widgets},
 	{NULL, 0, 0, 0, 0, 0, 0}
 };
 
@@ -129,6 +147,7 @@ void load_resource()
 	c_theme::add_color(COLOR_WND_PUSHED, GL_RGB(255, 255, 255));
 	c_theme::add_color(COLOR_WND_FOCUS, GL_RGB(25, 25, 25));
 	c_theme::add_color(COLOR_WND_BORDER, GL_RGB(255, 255, 255));
+	
 }
 
 void create_ui(void* phy_fb, int screen_width, int screen_height, int color_bytes) {
